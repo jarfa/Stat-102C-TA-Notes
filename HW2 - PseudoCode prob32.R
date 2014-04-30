274d11bb50dcc237a7b49892372efa1615dc87ec
Jonathan Arfa <jonathanarfa@gmail.com>
2014-04-30 11:39:53 -0700

    Fixed Prob on HW2 3.2

--- HW2 - PseudoCode prob32.R
+++ HW2 - PseudoCode prob32.R
@@ -4,16 +4,16 @@ x2 = rnewdist(Nsim, parameters)
 x3 = rnewdist(Nsim, parameters)
 X = cbind(x1, x2, x3)
 
-f = function(x1, x2, x3){
+f = function(x1, x2, x3, mu){
   #remember that x1, ... are I.I.D.
-  dnorm(x1) * dnorm(x2) * dnorm(x3)
+  dnorm(x1, mu) * dnorm(x2, mu) * dnorm(x3, mu)
 }
 g = function(x1, x2, x3){
   dnewdist(x1) * ...
 }
 M = apply(X, 1, function(x){max(x[1], ....)})
 weights = apply(X, 1, function(x){
-  f(...) / g(...)
+  f(..., mu) / g(...)
 })
 indicators = M > C
 mean(indicators * weights)
