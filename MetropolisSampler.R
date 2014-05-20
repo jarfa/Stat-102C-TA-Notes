34e98e53d63c461d1efd35f6559433292676d191
Jonathan Arfa <jonathanarfa@gmail.com>
2014-05-20 16:25:22 -0700

    Added comments to MetropolisSampler.r

--- MetropolisSampler.R
+++ MetropolisSampler.R
@@ -1,14 +1,22 @@
+#For this sampler we need
+#1) the pdf of the target distribution (what we're trying to explore)
+#2) the pdf of the proposal distribution q (how we find the next point in the 
+#Markov chain from the current point)
+#3) a function to generate points from the proposal distribution, given the current point
+
+#In this example we're trying to get samples from a N(0,1) using a N(0, 0.1). 
+#You can chang the proposal distribution if you like.
 geneq = function(x){ #this function gets numbers from our proposal distribution q
-  rnorm(1, mean=x, sd = 0.1)
+  rnorm(1, mean=x, sd = 0.1) #try changing the sd for this distribution
 }
 q = function(x, y){ #the pdf of q
-  dnorm(y, mean=x, sd=0.1)
+  dnorm(y, mean=x, sd=0.1)#try changing the sd for this distribution.
 }
 f = function(x){ #the pdf of f - the target distribution
   dnorm(x, mean=0, sd = 1)
 }
 
-Nsim = 3e5
+Nsim = 1e5
 x = rep(0, Nsim) #start at x[1]==0... this doesn't matter too much
 for(t in 1:(Nsim-1)){
   y = geneq(x[t])
@@ -28,10 +36,10 @@ y=rnorm(Nsim); qqnorm(y, main='QQplot of rnorm() Samples'); qqline(y, lwd=3, col
 #What if f() is not uni-modal?
 #Let's say that f() is rbeta(1/4, 1/4). We'll keep our q as normal, but narrow it to SD=0.25
 geneq = function(x){#this function gets numbers from our proposal distribution q
-  rnorm(1, mean=x, sd=0.25)
+  rnorm(1, mean=x, sd=0.05)
 }
 q = function(x, y){#the pdf of q
-  dnorm(y, mean=x, sd=0.25)
+  dnorm(y, mean=x, sd=0.05)
 }
 f = function(x){#the pdf of f - the target distribution
   dbeta(x, .25, .25)
@@ -50,3 +58,4 @@ for(t in 1:(Nsim-1)){
 }
 hist(x, freq=FALSE)
 curve(dbeta(x, 1/4, 1/4), add=TRUE, col='blue')
+#is the distribution lopsided? Why? Does increasing Nsim change that?
